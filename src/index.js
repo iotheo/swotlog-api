771ac94a0bee4b8d486875a5c37e2723ad545e8f
iotheo <ioan.theodo@gmail.com>
2019-07-27 20:17:59 +0300

    chore: remove unecessary comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello world1112')
+  res.send('Hello world')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
