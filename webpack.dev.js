cfcdb9cf135d2abf6825ca5547de9ffbefd07588
iotheo <ioan.theodo@gmail.com>
2019-07-27 19:46:15 +0300

    feat(Server): support ES6 syntax

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,4 +15,18 @@ module.exports = {
   plugins: [
     new NodemonPlugin(), 
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
