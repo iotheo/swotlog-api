726d700b3c869c31c719b5a444a8fe47dfb68a7f
John Theo <ioan.theodo@gmail.com>
2019-10-07 23:43:50 +0300

    chore(webpack config): remove unecessary paths

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,11 +4,7 @@ const NodemonPlugin = require('nodemon-webpack-plugin');
 module.exports = {
   mode: 'development',
   target: 'node',
-  entry: [
-    './src/index.js',
-    './src/api/Users.js',
-    './src/db.js',
-  ],
+  entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
