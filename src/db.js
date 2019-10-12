31626a38f42ea03c867c761819e782f5cab25b3f
John Theo <ioan.theodo@gmail.com>
2019-10-12 16:23:08 +0300

    chore(db): cleanup comments

--- src/db.js
+++ src/db.js
@@ -19,57 +19,6 @@ pool.on('error', (err, client) => {
   process.exit(-1);
 });
 
-// const createUser = (req, res) => {
-//     const { username, email, password } = req.body;
-
-//     pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
-// [username, email, password], (error, results) => {
-//       if (error) {
-//         throw error;
-//       }
-//       res.status(201).send(`User added with ID: ${results.insertId}`)
-//     });
-// }
-
-// const getUserById = (req, res) => {
-//     const id = parseInt(req.params.id)
-
-//     pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       res.status(200).json(results.rows)
-//     })
-//   }
-
-// const updateUser = (req, res) => {
-//     const id = parseInt(req.params.id)
-//     const { username, email, password } = req.body
-
-//     pool.query(
-//       'UPDATE users SET username = $1, email = $2, password = $3 WHERE id = $4',
-//       [username, email, password, id],
-//       (error, results) => {
-//         if (error) {
-//           throw error
-//         }
-//         res.status(200).send(`User modified with ID: ${id}`)
-//       }
-//     )
-//   }
-
-//   const deleteUser = (req, res) => {
-//     const id = parseInt(req.params.id)
-
-//     pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       res.status(200).send(`User deleted with ID: ${id}`)
-//     })
-//   }
-
 export {
   pool,
-  // getUsers,
 };
