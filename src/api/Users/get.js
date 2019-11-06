3ba7ceb3e7e3c6ffafea9911cb1f584590d94c4c
John Theo <ioan.theodo@gmail.com>
2019-11-06 03:22:35 +0200

    feat(Users): fetch user related posts, amongst other data

--- src/api/Users/get.js
+++ src/api/Users/get.js
@@ -4,19 +4,30 @@ const get = (req, res) => {
   const userId = req.params.id;
 
   if (parseInt(userId, 10)) {
-    pool.query('SELECT * FROM person where id = $1 LIMIT 1', [userId], (error, results) => {
-      if (error) {
-        throw error;
+    pool.query(
+      'SELECT * FROM person,\
+        (\
+          SELECT json_agg(to_json(fields)) AS posts\
+          from (\
+          select * FROM post WHERE person_id = $1\
+          ) fields\
+        ) AS posts\
+      WHERE person.id = $1 LIMIT 1',
+      [userId],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+
+        if (!results.rows.length) {
+          res.status(404).send('User not found :((');
+
+          return;
+        }
+
+        res.status(200).json(results.rows[0]);
       }
-
-      if (!results.rows.length) {
-        res.status(404).send('Not found. Bruh');
-
-        return;
-      }
-
-      res.status(200).json(results.rows);
-    });
+    );
 
     return;
   }
