f6981abf7bbc320c62c70d10bddf94335101fecd
John Theo <ioan.theodo@gmail.com>
2019-11-12 21:05:43 +0200

    chore(Classes Delete): keep consistency between endpoints

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ app.post('/tutors(/:id([0-9]+))?', Tutors.get);
 app.post('/login', passport.authenticate('local'), Users.login);
 app.post('/classes(/:id([0-9]+))?', Classes.get);
 app.post('/classes/create', Classes.create);
-app.delete('/classes/delete', Classes.del);
+app.delete('/classes/:id([0-9]+)', Classes.del);
 
 // app.delete('/users/:id', db.deleteUser);
 app.post('*', (req, res) => {
