1b09a8f228a0048db2bf701f2531756aa73b2dd2
John Theo <john@Johns-MacBook-Pro.local>
2019-11-25 21:37:54 +0200

    hotfix(Users): fix issues with getpassed and getsubscribed

--- src/api/Users/getSubscribed.js
+++ src/api/Users/getSubscribed.js
@@ -3,12 +3,12 @@ import { pool } from 'db';
 const getSubscribed = (req, res) => {
   const {
     id,
-  } = res.locals;
+  } = req.user;
 
   pool.query(
     `SELECT class.id, class.name
       FROM class
-      INNER JOIN class_student ON class.id = class.student_id
+      INNER JOIN class_student ON class.id = class_student.class_id
       INNER JOIN student ON class_student.student_id = student.id
       INNER JOIN person ON person.id = student.person_id
       WHERE person.id = $1 AND has_subscribed IS true`,
